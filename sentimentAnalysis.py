d6184407fe2beab9dd908bf7233604bca40f61ad
Manemuneee <phash1989@gmail.com>
2017-12-25 15:14:40 +0100

    exploratory

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -8,8 +8,24 @@ This is a temporary script file.
 import nltk
 import csv
 import pandas as pd
+import matplotlib
 
-dfTrain = pd.from_csv('trina.csv')
+
+
+dfTrain = pd.read_csv('C:/Users/Pedram/Documents/GitManemuneee/KagTox001/train.csv')
+
+dfTrain['toxic', ] = dfTrain['toxic'].astype('int')
+dfTrain['severe_toxic', ] = dfTrain['severe_toxic'].astype('int')
+dfTrain['obscene', ] = dfTrain['obscene'].astype('int')
+dfTrain['threat', ] = dfTrain['threat'].astype('int')
+dfTrain['insult', ] = dfTrain['insult'].astype('int')
+dfTrain['identity_hate', ] = dfTrain['identity_hate'].astype('int') 
+
+selCols = ['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
+dfTrain['overallToxicity'] = dfTrain[selCols].sum(axis = 1)
+
+dfTrain['overallToxicity'].plot(kind='hist')
+dfTrain[dfTrain['overallToxicity'] != 0]['overallToxicity'].plot(kind='hist') 
 
 
 
